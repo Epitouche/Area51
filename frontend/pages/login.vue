20ef8bdc939ace2b91f1f0c2ce2b328c3f2d6127
OxiiLB <romain.giraud@epitech.eu>
2024-12-02 11:33:37 +0100

    [UPD] size of card

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -7,7 +7,7 @@ import Button from "@/components/Button.vue";
     class="flex items-center justify-center min-h-screen bg-primaryWhite-500 dark:bg-fontBlack"
   >
     <div
-      class="w-full max-w-md p-8 space-y-16 bg-gradient-to-b from-tertiary-300 to-tertiary-500 dark:from-tertiary-500 dark:to-tertiary-300 text-fontWhite rounded-lg shadow-lg"
+      class="w-full max-w-md p-8 space-y-10 bg-gradient-to-b from-tertiary-500 to-tertiary-600 dark:from-tertiary-600 dark:to-tertiary-500 text-fontWhite rounded-lg shadow-lg"
     >
       <h2 class="text-2xl font-bold text-center">LOG IN</h2>
       <form class="space-y-6">
@@ -29,23 +29,24 @@ import Button from "@/components/Button.vue";
             class="w-full px-4 py-2 mt-1 dark:text-fontBlack bg-primaryWhite-300 border border-primaryWhite-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent-500 focus:border-accent-500"
           />
         </div>
-        <div class="flex items-center">
-          <input
-            id="remember"
-            type="checkbox"
-            class="w-4 h-4 text-accent-500 border-primaryWhite-300 rounded focus:ring-accent-500"
-          />
-          <label for="remember" class="ml-2 text-sm">Remember me</label>
-        </div>
-        <div class="flex justify-center">
-          <Button
-            text="Log In"
-            bgColor="bg-primaryWhite-500"
-            hoverColor="hover:bg-secondaryWhite-500"
-            textColor="text-fontBlack"
-          />
-        </div>
       </form>
+      <div class="flex items-center">
+        <input
+          id="remember"
+          type="checkbox"
+          class="w-4 h-4 text-accent-500 border-primaryWhite-300 rounded focus:ring-accent-500"
+        />
+        <label for="remember" class="ml-2 text-sm">Remember me</label>
+      </div>
+      <div class="flex justify-center">
+        <Button
+          text="Log In"
+          bgColor="bg-primaryWhite-500"
+          hoverColor="hover:bg-secondaryWhite-500"
+          textColor="text-fontBlack"
+        />
+      </div>
+      <hr class="border-primaryWhite-400" />
       <div class="flex justify-around space-x-4">
         <Button
           text="Github"
@@ -62,7 +63,7 @@ import Button from "@/components/Button.vue";
           textColor="text-fontBlack"
         />
       </div>
-      <p class="text-center text-sm mt-4">
+      <p class="text-center text-sm">
         <a href="#" class="text-fontWhite underline">Forgot password?</a>
       </p>
     </div>
