c0ea109c6134a9465ed04350025febdd38820629
OxiiLB <romain.giraud@epitech.eu>
2024-12-02 15:13:13 +0100

    [ADD] register page

---
+++ frontend/pages/Register.vue
@@ -0,0 +1,71 @@
+<script>
+import Button from "@/components/Button.vue";
+import Input from "@/components/Input.vue";
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-center min-h-screen bg-primaryWhite-500 dark:bg-fontBlack"
+  >
+    <div
+      class="w-full transform -translate-x-3/4 max-w-md p-8 space-y-10 bg-gradient-to-b from-tertiary-500 to-tertiary-600 dark:from-tertiary-600 dark:to-tertiary-500 text-fontWhite rounded-lg shadow-lg"
+    >
+      <h2 class="text-2xl font-bold text-center">REGISTER</h2>
+      <form class="space-y-6">
+        <div class="flex justify-around">
+          <Input id="name" type="text" label="Name" icon="fas fa-user" />
+          <Input
+            id="lastname"
+            type="text"
+            label="Lastname"
+            icon="fas fa-user"
+          />
+        </div>
+        <div>
+          <Input id="email" type="email" label="Email" icon="fas fa-user" />
+        </div>
+        <div>
+          <Input
+            id="password"
+            type="password"
+            label="Password"
+            icon="fas fa-lock"
+          />
+        </div>
+      </form>
+      <div class="flex items-center">
+        <input
+          id="remember"
+          type="checkbox"
+          class="w-4 h-4 text-accent-500 border-primaryWhite-300 rounded focus:ring-accent-500"
+        />
+        <label for="remember" class="ml-2 text-sm">Remember me</label>
+      </div>
+      <div class="flex justify-center">
+        <Button
+          text="Create Account"
+          bgColor="bg-primaryWhite-500"
+          hoverColor="hover:bg-secondaryWhite-500"
+          textColor="text-fontBlack"
+        />
+      </div>
+      <hr class="border-primaryWhite-400" />
+      <div class="flex justify-around space-x-4">
+        <Button
+          text="Github"
+          class="w-full"
+          bgColor="bg-primaryWhite-500"
+          hoverColor="hover:bg-secondaryWhite-500"
+          textColor="text-fontBlack"
+        />
+        <Button
+          text="Google"
+          class="w-full"
+          bgColor="bg-primaryWhite-500"
+          hoverColor="hover:bg-secondaryWhite-500"
+          textColor="text-fontBlack"
+        />
+      </div>
+    </div>
+  </div>
+</template>
