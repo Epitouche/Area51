25dbe658542bdd753bfaee988a1e06b1a8d749d5
karumapathetic <arthur.lassausaie@epitech.eu>
2024-12-03 15:48:12 +0100

    fix: removing serviceID to link directly with service

--- backend/schemas/actionsSchema.go
+++ backend/schemas/actionsSchema.go
@@ -5,10 +5,8 @@ import "time"
 type Action struct {
 	Id				uint64		`json:"id,omitempty" gorm:"primary_key;auto_increment"`
 	Name			string		`json:"name" gorm:"type:varchar(100)"`
-	ServiceId		uint64		`json:"id_service,omitempty"`
+	Service			Service		`json:"service,omitempty" gorm:"foreignkey:ServiceId;references:Id"`
 	Description		string		`json:"description" gorm:"type:varchar(100)"`
 	CreatedAt		time.Time	`json:"created_at" gorm:"default:CURRENT_TIMESTAMP"`
 	UpdatedAt		time.Time	`json:"updated_at" gorm:"default:CURRENT_TIMESTAMP"`
-
-	Service			Service		`json:"service,omitempty" gorm:"foreignkey:ServiceId;references:Id"`
 }
