c36f7adc9237a73e6f13559c5550a57652070571
karumapathetic <arthur.lassausaie@epitech.eu>
2024-12-03 16:30:53 +0100

    [FIX] changing the name of the Relation data table

--- backend/schemas/relationsSchema.go
+++ backend/schemas/relationsSchema.go
@@ -1,6 +1,6 @@
 package schemas
 
-type Relation struct {
+type UserServiceLink struct {
 	Id				uint64		`json:"id,omitempty" gorm:"primary_key;auto_increment"`
 	UserId			User		`json:"user,omitempty" gorm:"foreignkey:UserId;references:Id"`
 	ServiceId		Service		`json:"service,omitempty" gorm:"foreignkey:ServiceId;references:Id"`
