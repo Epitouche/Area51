dda03ede84f1304c23381f1f88a0f28fdcabb0d8
OxiiLB <romain.giraud@epitech.eu>
2025-01-15 10:12:44 +0000

    [UPD] themeSwitch.vue has no hydratation errors

--- frontend/components/ThemeSwitch.vue
+++ frontend/components/ThemeSwitch.vue
@@ -1,7 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+const colorMode = useColorMode();
+const isMounted = ref(false);
+
+onMounted(() => {
+    isMounted.value = true;
+});
+
+const toggleTheme = () => {
+    colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark';
+};
+</script>
+
 <template>
     <div class="flex flex-row items-center justify-center">
         <Icon
-            v-show="colorMode.preference === 'light' || colorMode.preference === 'system'"
+            v-if="isMounted && (colorMode.preference === 'light' || colorMode.preference === 'system')"
             name="mdi:weather-night"
             class="w-7 h-7 cursor-pointer transition-transform duration-100 hover:scale-120 text-typography_primary_light dark:text-typography_primary_dark"
             width="28"
@@ -9,7 +23,7 @@
             @click="toggleTheme"
         />
         <Icon
-            v-show="colorMode.preference === 'dark'"
+            v-if="isMounted && colorMode.preference === 'dark'"
             name="material-symbols:sunny-outline-rounded"
             class="w-7 h-7 cursor-pointer transition-transform duration-100 hover:scale-120 text-typography_primary_light dark:text-typography_primary_dark"
             width="28"
@@ -18,10 +32,3 @@
         />
     </div>
 </template>
-
-<script setup>
-const colorMode = useColorMode();
-const toggleTheme = () => {
-    colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark';
-};
-</script>
